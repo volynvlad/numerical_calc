63ef4b606b6f00dd023a07fe99a07d925e956db2
volynvlad <volynvlad@gmail.com>
2019-11-26 13:14:21 +0300

    changed ex9 ex10

--- ex10.m
+++ ex10.m
@@ -1,12 +1,12 @@
 clear;
 clc;
 
-eps = 10 ** (-6);
+eps = 10 ** (-12);
 
 # variant 7
 
-f1 = @(x,y) y - 0.5 * x ** 2 + x - 0.5;
-f2 = @(x,y) 2 * x + y - (1 / 6) * y ** 3 - 1.6;
+f1 = @(x,y) y - 0.5 * x .** 2 + x - 0.5;
+f2 = @(x,y) 2 * x + y - (1 / 6) * y .** 3 - 1.6;
 t = -10:0.1:10;
 [X,Y] = ndgrid(t, t);
 
@@ -21,25 +21,14 @@ hold on
 f1_prim_x = @(x, y) -x + 1;
 f1_prim_y = @(x, y) 1;
 f2_prim_x = @(x, y) 2;
-f2_prim_y = @(x, y) 1 - (y ** 2) / 2;
+f2_prim_y = @(x, y) 1 - (y .** 2) / 2;
 
 J = @(x, y) [f1_prim_x(x, y), f1_prim_y(x, y); f2_prim_x(x, y), f2_prim_y(x, y)];
 F = @(x, y) [f1(x, y); f2(x, y)];
 A = @(x, y) [f1(x, y), f1_prim_y(x, y); f2(x, y), f2_prim_y(x, y)];
 B = @(x, y) [f1_prim_x(x, y), f1(x, y); f2_prim_x(x, y), f2(x, y)];
 
-x0 = [3 1];
-
-det_A = f1_prim_x(x0(1), x0(2)) * f2_prim_y(x0(1), x0(2)) 
-      - f2_prim_x(x0(1), x0(2)) * f1_prim_y(x0(1), x0(2));
-
-alpha = - f2_prim_y(x0(1), x0(2)) / det_A
-betta =   f1_prim_y(x0(1), x0(2)) / det_A
-gamma =   f2_prim_x(x0(1), x0(2)) / det_A
-delta = - f1_prim_x(x0(1), x0(2)) / det_A
-
-phi1 = @(x,y) x + alpha * f1(x, y) + betta * f2(x, y);
-phi2 = @(x,y) y + gamma * f1(x, y) + delta * f2(x, y);
+x0 = [-3 3];
 
 x = x0;
 err = 1;
@@ -51,6 +40,8 @@ while err > eps
     #x = x_prev - F(x_prev(1), x_prev(2)) / det(J(x_prev(1), x_prev(2)));
     x(1) = x_prev(1) - det(A(x_prev(1), x_prev(2))) / det(J(x_prev(1), x_prev(2)));
     x(2) = x_prev(2) - det(B(x_prev(1), x_prev(2))) / det(J(x_prev(1), x_prev(2)));
+    plot(x(1), x(2), '.', 'MarkerSize', 25);
+    hold on
     err = norm(x_prev - x);
     errs(1, k) = k;
     errs(2, k) = err;
--- ex9.m
+++ ex9.m
@@ -5,8 +5,8 @@ eps = 10 ** (-6);
 
 # variant 7
 
-f1 = @(x,y) y - 0.5 * x ** 2 + x - 0.5;
-f2 = @(x,y) 2 * x + y - (1 / 6) * y ** 3 - 1.6;
+f1 = @(x,y) y - 0.5 * x .** 2 + x - 0.5;
+f2 = @(x,y) 2 * x + y - (1 / 6) * y .** 3 - 1.6;
 X = -5:0.1:5;
 Y = X;
 [X,Y] = meshgrid(X,Y);
@@ -16,7 +16,7 @@ contour(X, Y, f1(X,Y), [0,0])
 hold on
 contour(X, Y, f2(X,Y), [0,0])
 hold on
-x0 = [3 3];
+x0 = [-1 3];
 f1_prim_x = @(x, y) -x + 1;
 f1_prim_y = @(x, y) 1;
 f2_prim_x = @(x, y) 2;
@@ -43,6 +43,8 @@ while err > eps
     x(1) = phi1(x(1), x(2));
     x(2) = phi2(x(1), x(2));
     err = norm(x_prev - x);
+    plot(x(1), x(2), '.', 'MarkerSize', 25);
+    hold on
     errs(1,k) = k;
     errs(2,k) = err;
     k = k + 1;
@@ -50,6 +52,7 @@ end
 display(k);
 display(x);
 plot(errs(1, :), errs(2, :));
+hold on
 #semilogy(errs(1, :), errs(2, :));
 
 
